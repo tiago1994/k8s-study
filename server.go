c4755e38219f76379708c1b08b89372400f26405
Tiago Matos <tiago.almeida@sensedia.com>
2022-03-18 13:45:05 -0300

    feat: created project and small k8s yaml configuration

---
+++ server.go
@@ -0,0 +1,12 @@
+package main
+
+import "net/http"
+
+func main() {
+	http.HandleFunc("/", Home)
+	http.ListenAndServe(":3000", nil)
+}
+
+func Home(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("<h1>Chegou aqui</h1>"))
+}
